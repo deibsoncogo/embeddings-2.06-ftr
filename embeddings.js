cc4508c22c7e36f1acdad07f071b103ba2fe47c3
Deibson Cogo <deibsoncogo@outlook.com>
2025-10-24 20:00:18 -0300

    feat: criação do modelo embeddings

---
+++ embeddings.js
@@ -0,0 +1,9 @@
+import { pipeline } from "@huggingface/transformers"
+
+const embedder = await pipeline(
+  "feature-extraction",
+  "Xenova/all-MiniLM-L6-v2",
+  { dtype: "q8"}
+)
+
+console.log("embedder =>", await embedder("oi tudo bem?"))
