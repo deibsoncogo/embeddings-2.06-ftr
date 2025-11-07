38737af0c382cca28cb94e87f06fc115e03398ba
Deibson Cogo <deibsoncogo@outlook.com>
2025-11-06 22:18:00 -0300

    fix: correção na criação dos dados no banco

--- vector_database/data-loader/load-data.js
+++ vector_database/data-loader/load-data.js
@@ -6,16 +6,6 @@ const chromaClient = new ChromaClient()
 
 const collection = await chromaClient.getOrCreateCollection({ name: "movies" })
 
-await collection.add({
-  ids: ["1", "2", "3"],
-  documents: ["Olá", "Oi", "Tudo bem"],
-  metadatas: [
-    { "title": "Meu Filme" },
-    { "name": "Arthur" },
-    { "age": 20 },
-  ]
-})
-
 const ids = []
 const documents = []
 const metadatas = []
@@ -23,30 +13,28 @@ const metadatas = []
 fs.createReadStream("mpst-full-data.csv")
   .pipe(csv())
   .on("data", (row) => {
-    const document = {
-      "title": row["title"], "tags": row["tags"], "synopsis": row["synopsis"]
-    }
+      const document = { "title": row["title"], "tags": row["tags"], "synopsis": row["plot_synopsis"] }
 
-    ids.push(row["imdb_id"])
-    documents.push(JSON.stringify(document))
-    metadatas.push(document)
+      ids.push(row["imdb_id"])
+      documents.push(JSON.stringify(document))
+      metadatas.push(document)
   })
   .on("end", async () => {
-    let startIdx = 0
+      let startIdx = 0
 
-    while (startIdx < ids.length) {
-      let endIdx = startIdx + 500
+      while (startIdx < ids.length) {
+          let endIdx = startIdx + 500
 
-      console.log(`Adding documents from ${startIdx} to ${endIdx}`)
+          console.log(`Adding documents from ${startIdx} to ${endIdx}`)
 
-      await collection.add({
-        ids: ids.slice(startIdx, endIdx),
-        documents: documents.slice(startIdx, endIdx),
-        metadatas: metadatas.slice(startIdx, endIdx),
-      })
+          await collection.add({
+              ids: ids.slice(startIdx, endIdx),
+              documents: documents.slice(startIdx, endIdx),
+              metadatas: metadatas.slice(startIdx, endIdx),
+          })
 
-      startIdx = endIdx
-    }
+          startIdx = endIdx
+      }
 
-    console.log("Done!")
+      console.log("Done!")
   })
