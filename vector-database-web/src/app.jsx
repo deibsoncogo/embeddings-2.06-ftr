95d4b0bd7464f7c60d5006c3a5e0a17c274ccc77
Deibson Cogo <deibsoncogo@outlook.com>
2025-11-17 20:19:10 -0300

    feat: deixando o projeto bonito

--- vector-database-web/src/app.jsx
+++ vector-database-web/src/app.jsx
@@ -1,6 +1,7 @@
 import { ChromaClient } from "chromadb"
 import { env } from "chromadb-default-embed"
 import { useEffect, useState } from "react"
+import movieMock from "../movie-mock.json"
 import "./app.css"
 
 env.useBrowserCache = false
@@ -10,23 +11,52 @@ function MovieTag({ tag }) {
   return <div className="movie-tag">{tag}</div>
 }
 
-function MovieCard({ title, tags, synopsis }) {
+function MovieCard({ id, title, tags, synopsis, distance, img }) {
+  const apiKey = import.meta.VITE_OMDB_API_KEY
+  const src = apiKey ? `https://img.omdbapi.com/?apiKey=${apiKey}&i=${id}` : img
+
   return (
     <div className="movie-card">
-      <h2 className="movie-title">{title}</h2>
-      <div className="tag-container">
-        <div className="tag-holder">
-          {tags.map((tag, index) => <MovieTag key={index} tag={tag} />)}
+      <img className="movie-card-img" src={src} />
+
+      <div className="movie-info">
+        <div className="title-container">
+          <h2 className="movie-title">
+            <a href={`https:www.imdb.com/title/${id}`} target="_blank">
+              {title}
+            </a>
+          </h2>
+
+          <div className="title-background" style={{width: `${distance*100}%`}} />
         </div>
+
+        <b>Tags</b>
+
+        <div className="tag-container">
+          <div className="tag-holder">
+            {tags.map((t, idx) => <MovieTag key={idx} tag={t} />)}
+          </div>
+        </div>
+
+        <b>Synopsis</b>
+
+        <div className="synopsis">{synopsis}</div>
       </div>
-      <div className="synopsis">{synopsis}</div>
+    </div>
+  )
+}
+
+function LoadingIndicator(){
+  return (
+    <div className="loading-container">
+      Loading...
     </div>
   )
 }
 
 export function App() {
   const [chromaCollection, setChromaCollection] = useState(null)
-  const [movieList, setMovieList] = useState([])
+  const [movieList, setMovieList] = useState(movieMock)
   const [isLoading, setIsLoading] = useState(false)
   const [isConnected, setIsConnected] = useState(false)
   const [queryText, setQueryText] = useState("")
@@ -35,14 +65,15 @@ export function App() {
     setIsLoading(true)
 
     try {
-      const res = await chromaCollection.query({ queryTexts: [queryText] })
-
+      const res = await chromaCollection.query({ queryTexts: queryText })
       const movies = []
+
+      const distances = res.distances[0].map(d => ((1-d)+1)/2)
 
       for (let i = 0; i < res.ids[0].length; i++) {
         movies.push({
           id: res.ids[0][i],
-          distance: res.distances[0][i],
+          distance: distances[i],
           title: res.metadatas[0][i].title,
           tags: res.metadatas[0][i].tags.split(", "),
           synopsis: res.metadatas[0][i].synopsis,
@@ -60,7 +91,10 @@ export function App() {
   useEffect(() => {
     const initializeChroma = async () => {
       const chromaClient = new ChromaClient()
-      const collection = await chromaClient.getOrCreateCollection({ name: "movies" })
+
+      const collection = await chromaClient.getOrCreateCollection({
+        name: "movies", space: "cosine"
+      })
 
       setChromaCollection(collection)
       setIsConnected(true)
@@ -74,17 +108,17 @@ export function App() {
       <h1>Movie Recommender</h1>
 
       <div className="form-container">
-        <textarea type="text" onChange={e => setQueryText(e.target.value)} />
+        <textarea onChange={e => setQueryText(e.target.value)} />
 
         <button onClick={queryDatabase} disabled={!isConnected || isLoading}>
           Submit
         </button>
       </div>
 
-      {isLoading && <p>Loading...</p>}
+      {isLoading ? <LoadingIndicator /> : null}
 
       <div className="movie-list">
-        {movieList.map((movie, index) => <MovieCard key={index} {...movie} />)}
+        {movieList.map((m, idx) => <MovieCard key={idx} {...m} />)}
       </div>
     </>
   )
