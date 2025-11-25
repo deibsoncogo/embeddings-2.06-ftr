6a36483c06ec33082b97e47880e8a846d7e60d84
Deibson Cogo <deibsoncogo@outlook.com>
2025-11-25 20:29:59 -0300

    feat: fazendo chamadas para gemini api

---
+++ llm/classify.js
@@ -0,0 +1,28 @@
+import { createPartFromUri, createUserContent, GoogleGenAI } from "@google/genai"
+import dotenv from "dotenv"
+import fs from "node:fs"
+
+dotenv.config()
+
+const genai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
+
+const embeddings = JSON.parse(fs.readFileSync("../nearest-neighbors/embeddings.json"))
+
+const testEmbeddings = embeddings
+  .filter(e => e["split"] == "test")
+  .map(e => "../nearest-neighbors" + e["path"].slice(1))
+
+const image = await genai.files.upload({
+  file: testEmbeddings[0],
+  config: { mimeType: "image/jpeg" }
+})
+
+const response = await genai.models.generateContent({
+  model: "gemini-2.0-flash",
+  contents: createUserContent([
+    createPartFromUri(image.uri, image.mimeType),
+    "faça uma descrição da imagem acima"
+  ])
+})
+
+console.log(response.text)
