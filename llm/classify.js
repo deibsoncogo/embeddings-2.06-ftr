42b3bcffb4645ced9f163913c3d7f8aae79a24a0
Deibson Cogo <deibsoncogo@outlook.com>
2025-11-25 21:01:10 -0300

    feat: organizando código

--- llm/classify.js
+++ llm/classify.js
@@ -1,6 +1,7 @@
 import fs from "fs"
 import { createPartFromUri, createUserContent, GoogleGenAI } from "@google/genai"
 import dotenv from "dotenv"
+import { type } from "os"
 
 dotenv.config()
 
@@ -8,42 +9,58 @@ const genai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
 
 const embeddings = JSON.parse(fs.readFileSync("../nearest-neighbors/embeddings.json"))
 
-const testEmbeddings = embeddings
+const testPaths = embeddings
   .filter(e => e["split"] == "test")
   .map(e => "../nearest-neighbors" + e["path"].slice(1))
 
-const image = await genai.files.upload({
-  file: testEmbeddings[50],
-  config: { mimeType: "image/jpeg" }
-})
+function readImg(path) {
+  return fs.readFileSync(path, { encoding: "base64" })
+}
 
-const response = await genai.models.generateContent({
-  model: "gemini-2.0-flash",
-  contents: createUserContent([
-    createPartFromUri(image.uri, image.mimeType),
-    `
-    Identifique se a imagem contém gatos ou cachorros.
-    Retorne uma das seguintes categorias de acordo com o conteúdo da imagem:
-    'cat' caso a imagem contenha um ou mais gatos ou
-    'dog' caso a imagem contenha um ou mais cachorros
-    também retorne a cor do animal identificado.
-    `
-  ]),
-  config: {
-    responseMimeType: "application/json",
-    responseSchema: {
-      type: "array",
-      items: {
-        type: "object",
-        properties: {
-          category: {
-            type: "string",
-            enum: ["dog", "cat"]
-          }
+function toInlineData(imgBase64) {
+  return { inlineData: { mimeType: "image/jpeg", data: imgBase64 } }
+}
+
+const prompt = `
+  Identifique se a imagem contém gatos ou cachorros.
+  Retorne uma das seguintes categorias de acordo com o conteúdo da imagem:
+  "cat" caso a imagem contenha um ou mais gatos ou
+  "dog" caso a imagem contenha um ou mais cachorro
+`
+
+const outputConfig = {
+  responseMimeType: "application/json",
+  responseSchema: {
+    type: "array",
+    items: {
+      type: "object",
+      properties: {
+        category: {
+          type: "string",
+          enum: ["dog", "cat"]
         }
       }
     }
   }
-})
+}
 
-console.log(response.text)
+async function geminiRequest(contents) {
+  const response = await genai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: contents,
+    config: outputConfig
+  })
+
+  return response
+}
+
+async function llmClassifier(path) {
+  const imgBase64 = readImg(path)
+  const imgInlineData = toInlineData(imgBase64)
+  const contents = [imgInlineData, { text: prompt }]
+  const response = await geminiRequest(contents)
+
+  return JSON.parse(response.text)[0]["category"]
+}
+
+console.log(await llmClassifier(testPaths[0]))
