d0de1597328a7aa714225edabfb4013cc682a405
Deibson Cogo <deibsoncogo@outlook.com>
2025-12-02 19:44:24 -0300

    feat: finalizando a aplicação

---
+++ classifier-llm-web/src/components/captioned-image.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { classificationRequest } from "../classify/api"
+
+export function CaptionedImage({ imgSrc, method }) {
+  const [category, setCategory] = useState(null)
+
+  useEffect(() => {
+    setCategory(null)
+
+    const getCaption = async () => {
+      const response = await classificationRequest(method, imgSrc)
+      setCategory(response["category"])
+    }
+
+    getCaption()
+  }, [imgSrc, method])
+
+  return <>
+    <div className="captioned-image">
+      {category === null ? <h2>It"s a ...</h2> : <h2>It"s a {category}!!!</h2>}
+      <img className="image" src={imgSrc} />
+    </div>
+  </>
+}
